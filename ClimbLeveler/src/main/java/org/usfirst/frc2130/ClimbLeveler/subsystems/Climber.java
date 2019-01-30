499ee2e77f2d13a2374d7e967fa7ed46542d6daa
Jake Katz <idahopotato@gmail.com>
2019-01-29 23:02:41 -0800

    Added code for climb leveler control logic.

--- ClimbLeveler/src/main/java/org/usfirst/frc2130/ClimbLeveler/subsystems/Climber.java
+++ ClimbLeveler/src/main/java/org/usfirst/frc2130/ClimbLeveler/subsystems/Climber.java
@@ -1,12 +1,11 @@
-package org.usfirst.frc2130.DemoLeveler.subsystems;
+package org.usfirst.frc2130.ClimbLeveler.subsystems;
 
-import org.usfirst.frc2130.DemoLeveler.commands.*;
-import org.usfirst.frc2130.DemoLeveler.subsystems.NavXSubsystem;
+import org.usfirst.frc2130.ClimbLeveler.commands.*;
+import org.usfirst.frc2130.ClimbLeveler.subsystems.NavX;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -14,21 +13,25 @@ public class Climber extends PIDSubsystem {
 
     private Talon front;
     private Talon back;
-    private NavXSubsystem navX;
+    private NavX navX;
 
+    private double collectiveOutput;
 
     // Initialize your subsystem here
-    public Climber(NavXSubsystem navX) {
+    public Climber(NavX navX) {
+        // @TODO: These gains need to be updated. Start with P gain.
         super("Climber", 1.0, 0.0, 0.0);
 
         //Set local refernce to NavX for getting pitch command
         this.navX = navX;
 
+        // Initialize collective output to disable climbing
+        collectiveOutput = 0.0;
+
         setAbsoluteTolerance(0.2);
         getPIDController().setContinuous(false);
         getPIDController().setName("Climber", "PIDSubsystem Controller");
         LiveWindow.add(getPIDController());
-
 
         front = new Talon(0);
         addChild("Front", front);
@@ -38,12 +41,8 @@ public class Climber extends PIDSubsystem {
         addChild("Back", back);
         back.setInverted(false);
         
-
-
-        // Use these to get going:
-        // setSetpoint() -  Sets where the PID controller should move the system
-        //                  to
-        // enable() - Enables the PID controller.
+        // Make sure the system is off to start with
+        disable();
     }
 
     @Override
@@ -57,24 +56,58 @@ public class Climber extends PIDSubsystem {
 
     @Override
     protected double returnPIDInput() {
-        // Return your input value for the PID loop
-        // e.g. a sensor, like a potentiometer:
-        // yourPot.getAverageVoltage() / kYourMaxVoltage;
-
-        return navX.pidGet();
+        // This may need to be inverted to get the correct direction.
+        //
+        // Assume here that positive pitch means the the front of the robot
+        // is tilted up, that means that the front motor needs to slow down.
+        return navX.navXPitch();
 
     }
 
     @Override
     protected void usePIDOutput(double output) {
-        // Use output to drive your system, like a motor
-        // e.g. yourMotor.set(output);
+        // The PID output represents the controller's attempt to compensate for
+        // perceived pitch error. Following our convention for positive pitch
+        // meaning the robot is tilted up, for a simple P contoller:
+        //
+        //      output = Kp * error = Kp * (target - actual)
+        //      output = Kp * (0 - pitch)
+        //
+        // So we see that if there is positive pitch, the controller output will
+        // be negative. That means we should ADD the output of the controller to the
+        // front motor and SUBTRACT the output from the back motor.
 
-        front.pidWrite(output);
+        // Add controller output on front to decrease speed if pitch is positive
+        front.set(collectiveOutput + output);
 
+        // Subtract controller output on back to increase speed if pitch is positive
+        back.set(collectiveOutput - output);
+        
     }
 
+    public void startClimb(double collectiveCommand) {
+        // Starts climbing by setting the collective command and enabling the controller.
+        //
+        // By default, we target 0 tilt. If we want to get fancy we may want to maintain the
+        // orientation the robot is when it starts to climb by remembering the pitch when the
+        // command is sent. It may be more desirable for it to self-level in the end though.
 
+        // Target 0 pitch
+        setSetpoint(0.0);
 
+        // Set the internal collective output
+        collectiveOutput = collectiveCommand;
+
+        // Start the PID controller
+        enable();
+    }
+
+    public void stopClimb() {
+        // Turn everything off
+        disable();
+        front.set(0.0);
+        back.set(0.0);
+        collectiveOutput = 0.0;
+    }
 
 }
